152698f9eba485cbef7ced5835e2da53a040bcd1
Chris O'Haver <cohaver@infoblox.com>
2019-07-29 13:02:08 -0400

    code moved

--- kubernetes/corefile-tool/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/coredns/deployment/kubernetes/corefile-tool/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- kubernetes/migration/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/coredns/deployment/kubernetes/migration
-
-go 1.12
