30a683770a98aeb66b70d9faf571afca430960a9
Ngari Ndung'u <ngari.ndungu@gmail.com>
2019-10-30 18:35:47 +0300

    fix typos on kubernetes deployment scripts (#202)
    Signed-off-by: Ngari Ndung'u <ngari.ndungu@gmail.com>

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,10 +6,10 @@ show_help () {
 cat << USAGE
 usage: $0 [ -r REVERSE-CIDR ] [ -i DNS-IP ] [ -d CLUSTER-DOMAIN ] [ -t YAML-TEMPLATE ]
 
-    -r : Define a reverse zone for the given CIDR. You may specifcy this option more
+    -r : Define a reverse zone for the given CIDR. You may specify this option more
          than once to add multiple reverse zones. If no reverse CIDRs are defined,
          then the default is to handle all reverse zones (i.e. in-addr.arpa and ip6.arpa)
-    -i : Specify the cluster DNS IP address. If not specificed, the IP address of
+    -i : Specify the cluster DNS IP address. If not specified, the IP address of
          the existing "kube-dns" service is used, if present.
     -s : Skips the translation of kube-dns configmap to the corresponding CoreDNS Corefile configuration.
 
