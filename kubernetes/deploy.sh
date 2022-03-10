42a53ff6ec128cbef1d840bc2d193ffebbc454ba
zhaojul <zhaojun.li@hotmail.com>
2022-03-10 11:11:01 +0800

    add coredns image reg and tag
    Signed-off-by: zhaojul <zhaojun.li@hotmail.com>

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -4,7 +4,7 @@
 
 show_help () {
 cat << USAGE
-usage: $0 [ -r REVERSE-CIDR ] [ -i DNS-IP ] [ -d CLUSTER-DOMAIN ] [ -t YAML-TEMPLATE ]
+usage: $0 [ -r REVERSE-CIDR ] [ -i DNS-IP ] [ -d CLUSTER-DOMAIN ] [ -m COREDNS_IMAGE ] [ -v COREDNS_VERSION ]  [ -t YAML-TEMPLATE ]
 
     -r : Define a reverse zone for the given CIDR. You may specify this option more
          than once to add multiple reverse zones. If no reverse CIDRs are defined,
@@ -12,6 +12,8 @@ usage: $0 [ -r REVERSE-CIDR ] [ -i DNS-IP ] [ -d CLUSTER-DOMAIN ] [ -t YAML-TEMP
     -i : Specify the cluster DNS IP address. If not specified, the IP address of
          the existing "kube-dns" service is used, if present.
     -s : Skips the translation of kube-dns configmap to the corresponding CoreDNS Corefile configuration.
+    -m : coredns image registry (i.e. docker.io/coredns/coredns)
+    -v : coredns image tag (i.e. 1.9.0)
 
 USAGE
 exit 0
@@ -23,6 +25,8 @@ CLUSTER_DOMAIN=cluster.local
 YAML_TEMPLATE="$DIR/coredns.yaml.sed"
 STUBDOMAINS=""
 UPSTREAM=\\/etc\\/resolv\.conf
+COREDNS_IMAGE="coredns/coredns"
+COREDNS_VERSION="1.9.0"
 
 # Translates the kube-dns ConfigMap to equivalent CoreDNS Configuration.
 function translate-kube-dns-configmap {
@@ -79,7 +83,7 @@ function kube-dns-stubdomains-to-coredns {
 
 
 # Get Opts
-while getopts "hsr:i:d:t:k:" opt; do
+while getopts "hsr:i:d:m:v:t:k:" opt; do
     case "$opt" in
     h)  show_help
         ;;
@@ -90,6 +94,10 @@ while getopts "hsr:i:d:t:k:" opt; do
     i)  CLUSTER_DNS_IP=$OPTARG
         ;;
     d)  CLUSTER_DOMAIN=$OPTARG
+        ;;
+    m)  COREDNS_IMAGE="$OPTARG"
+        ;;
+    v)  COREDNS_VERSION="$OPTARG"
         ;;
     t)  YAML_TEMPLATE=$OPTARG
         ;;
@@ -118,6 +126,8 @@ replace=$'\\\n'
 sed -e "s/CLUSTER_DNS_IP/$CLUSTER_DNS_IP/g" \
     -e "s/CLUSTER_DOMAIN/$CLUSTER_DOMAIN/g" \
     -e "s?REVERSE_CIDRS?$REVERSE_CIDRS?g" \
+    -e "s?COREDNS_IMAGE?$COREDNS_IMAGE?g" \
+    -e "s/COREDNS_VERSION/$COREDNS_VERSION/g" \
     -e "s@STUBDOMAINS@${STUBDOMAINS//$orig/$replace}@g" \
     -e "s/UPSTREAMNAMESERVER/$UPSTREAM/g" \
     "${YAML_TEMPLATE}"
